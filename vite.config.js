7eba7d468ffbabeca639b01b68c20bd3a34c0b8e
joshna jojo <joshnajojo200621@gmail.com>
2025-06-16 10:06:09 +0530

    Initial commit with welcome screen

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-});
+  server: {
+    host: true, // ← this allows external access
+    port: 5173  // optional, default is 5173
+  }
+})
